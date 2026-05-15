cd9573af4df405a58859007834cb986a53ae1d80
cuongdq-dev <dqcuong191@gmail.com>
2026-05-15 16:05:33 +0700

    fix bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { configLoads } from './config';
 import { InsightSyncModule } from './modules/insight-sync/insight-sync.module';
+import { LarkSyncModule } from './modules/lark-sync/lark-sync.module';
+import { MetaSyncModule } from './modules/meta-sync/meta-sync.module';
 
 export const global_modules = [
   ScheduleModule.forRoot(),
