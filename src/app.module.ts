e736f9b6c3f9e35f0b35194d31e329ff9c158146
DangQuocCuong <dqcuong@tvhs.asia>
2026-05-25 17:00:52 +0700

    feat: fix insight duplication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { configLoads } from './config';
-import { MediaSyncModule } from './modules/media-sync/media-sync.module';
 
 export const global_modules = [
   ScheduleModule.forRoot(),
@@ -26,18 +25,16 @@ export const global_modules = [
 ];
 
 import { InsightSyncModule } from './modules/insight-sync/insight-sync.module';
-import { LarkSyncModule } from './modules/lark-sync/lark-sync.module';
 import { MetaApiModule } from './modules/meta-api/meta-api.module';
-import { MetaSyncModule } from './modules/meta-sync/meta-sync.module';
 
 @Module({
   imports: [
     ...global_modules,
     MetaApiModule,
     InsightSyncModule,
-    MetaSyncModule,
-    LarkSyncModule,
-    MediaSyncModule,
+    // MetaSyncModule,
+    // LarkSyncModule,
+    // MediaSyncModule,
   ],
 })
 export class AppModule implements OnModuleInit {
